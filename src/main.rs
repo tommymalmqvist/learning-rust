35552545b00ff874ed0c5a56ba132c2163a62d0e
Tommy Malmqvist <tommy.malmqvist@zebware.com>
2020-09-17 21:59:49 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::fmt;
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        if self.height > other.height {
+            if self.width > other.width {
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }
+}
+
+impl fmt::Display for Rectangle {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "width: {}, height: {}", self.width, self.height)
+    }
+}
+
+fn main() {
+    let r1: Rectangle = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let r2: Rectangle = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let r3: Rectangle = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2: {}", r1.can_hold(&r2));
+    println!("Can rect2 hold rect3: {}", r2.can_hold(&r3));
+}
